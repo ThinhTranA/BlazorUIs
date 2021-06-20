31bfd442d7ae18133ede2bb78df04fd7fdbaa060
Thinh Tran <48557139+ThinhTranA@users.noreply.github.com>
2021-06-20 21:11:00 +0800

    Adding PokeDex component.

---
+++ Pokedex/Pokedex/Models/Pokemon.cs
@@ -0,0 +1,10 @@
+namespace Pokedex.Models
+{
+    public class Pokemon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int BaseExperience { get; set; }
+    }
+}
